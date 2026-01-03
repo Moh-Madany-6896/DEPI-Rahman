be432ddc96e2af7038332c6e0901c8a7375b7e3c
Moh-Madany-6896 <Moh-Madany-6896>
2026-01-03 16:07:52 +0200

    s

---
+++ src/Python/Session_1/Code/loginUI.py
@@ -0,0 +1,27 @@
+import random
+
+def login_sys ():
+    user = "test"
+    password = "1111"
+    while True:
+        x = input ("Enter Your Username:  ")
+        if x == user:
+            y = input ("Enter Your password:  ")
+            if y == password:
+                v = random.randrange (1000, 10000)
+                print (f"Your Code is {v}")
+                while True:
+                    e = int (input ("Enter Your code: "))
+                    if e == v:
+                        print ("Access Success!!")
+                        break
+                    else:
+                        print ("Code is Incorrect!!!!, please try again")
+                        break
+                break
+            else:
+                print ("Password is Incorrect!!!!, please try again")
+                continue
+        else:
+            print ("username is Incorrect!!!!, please try again")
+            continue
